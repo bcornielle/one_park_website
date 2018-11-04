549ed8a63744ec6267bb58cffb8f5a15cda8cf90
Krodox <julian@krodox.com>
2018-11-04 12:20:30 -0500

    HR Listing Jobs

--- site/addons/APIAddon/APIAddonListener.php
+++ site/addons/APIAddon/APIAddonListener.php
@@ -1,17 +1,49 @@
 <?php
 namespace Statamic\Addons\APIAddon;
 use Illuminate\Support\Facades\Log;
+use Statamic\Events\Data\EntryDeleted;
 use Statamic\Events\Data\EntrySaved;
 use Statamic\Extend\Listener;
 class APIAddonListener extends Listener {
     public $events = [
-	    EntrySaved::class => 'saved'
+	    EntrySaved::class => 'saved',
+	    EntryDeleted::class => 'deleted',
     ];
 	public function saved(EntrySaved $event) {
 		$context = $event->contextualData();
 		if ($context['collection'] === 'careers'){
-			Log::info(print_r($context,true));
+			$context['posting_id'] = $context['slug'];
+			$context['entry_id'] = $context['id'];
+			$this->api_save($context);
 		}
 		return $event;
+	}
+	public function deleted(EntryDeleted $event) {
+		$context = $event->contextualData();
+		$this->api_delete($context['id']);
+		return $event;
+	}
+
+	private function api_save($career){
+		$url = env('API_URL') . env('API_VERSION') . '/jobs?api_token='.env('API_TOKEN');
+		$method = "PUT";
+		return $this->sync($url,$method,json_encode($career));
+	}
+	private function api_delete($entry_id){
+		$url = env('API_URL') . env('API_VERSION') . '/jobs/' . $entry_id . '?api_token='.env('API_TOKEN');
+		$method = "DELETE";
+		return $this->sync($url,$method,$entry_id);
+	}
+	private function sync($url,$method,$post_fields){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL,$url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST,$method);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'Content-Type: application/json'
+		));
+		curl_setopt($ch, CURLOPT_POSTFIELDS,$post_fields);
+		return curl_exec($ch);
 	}
 }
