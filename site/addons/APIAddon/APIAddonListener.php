234704d935dd95777cc30c63a982b399e62db4b9
Krodox <julian@krodox.com>
2019-05-06 21:11:50 -0400

    Formstack changes

--- site/addons/APIAddon/APIAddonListener.php
+++ site/addons/APIAddon/APIAddonListener.php
@@ -8,11 +8,29 @@ class APIAddonListener extends Listener {
     public $events = [
 	    EntrySaved::class => 'EntrySaved',
 	    EntryDeleted::class => 'EntryDeleted',
-	    'Form.submission.creating' => 'creating',
+	    'Form.submission.creating' => 'submissionCreating',
 	    'Form.submission.created' => 'submissionCreated',
     ];
-	//FormStack API
-	public function creating(Submission $submission) {
+    //CP Entries
+	public function EntrySaved(EntrySaved $event) {
+		$context = $event->contextualData();
+		if ($context['collection'] === 'careers'){
+			$context['entry_id'] = $context['id'];
+			$context['status_id'] = $context['status'];
+			$context['office_id'] = $context['office'];
+			$url = env('API_URL') . env('API_VERSION') . '/careers?api_token='.env('API_TOKEN');
+			$this->curl_api($url,"PUT",json_encode($context));
+		}
+		return $event;
+	}
+	public function EntryDeleted(EntryDeleted $event) {
+		$context = $event->contextualData();
+		$url = env('API_URL') . env('API_VERSION') . '/careers/' . $context['id'] . '?api_token='.env('API_TOKEN');
+		$this->curl_api($url,"DELETE",$context['id']);
+		return $event;
+	}
+	//Submissions
+	public function submissionCreating(Submission $submission) {
 		$errors = array();
 		$form_name = $submission->formset()->name();
 		if ($form_name === 'landing-page'){
@@ -34,6 +52,195 @@ class APIAddonListener extends Listener {
 			'errors' => $errors
 		];
 	}
+	public function submissionCreated(Submission $submission) {
+		$form_name = $submission->formset()->name();
+		if ($form_name === 'landing-page'){
+			$params = array();
+			$name = $submission->get('name');
+			$names = explode(' ', $name);
+			$params['fname'] = (isset($names[0])) ? $names[0] : null;
+			$params['lname'] = (isset($names[1])) ? $names[1] : 'lastname';
+			$params['phone'] = $submission->get('phone');
+			$params['company_phone'] = $submission->get('phone');
+			$params['email'] = $submission->get('email');
+			$params['company_name'] = $submission->get('business_name');
+			$params['company_has_banking'] = $submission->get('has_business_bank_account');
+			$params['company_active_bankruptcy'] = $submission->get('active_bankruptcy');
+			$revenue = $submission->get('monthly_revenue');
+			if ($revenue == 1) $revenue = 2500;
+			$params['company_revenue'] = $revenue;
+			//tracking
+			$params['source'] = $submission->get('source');
+			$campaign = null;
+			if (isset($_COOKIE['Visitor_Campaign__c'])) {
+				$campaign = $_COOKIE['Visitor_Campaign__c'] ;
+			} else {
+				if ($submission->get('utm_campaign')){
+					$campaign = $submission->get('utm_campaign');
+				}
+			}
+			$params['utm_campaign'] = $campaign;
+			$source = null;
+			if (isset($_COOKIE['Source__c'])) {
+				$source = $_COOKIE['Source__c'] ;
+			}else{
+				if ($submission->get('utm_source')){
+					$source = $submission->get('utm_source');
+				}
+			}
+			$params['utm_source'] = $source;
+			$medium = null;
+			if (isset($_COOKIE['Visitor_Medium__c'])) {
+				$medium = $_COOKIE['Visitor_Medium__c'] ;
+			}else{
+				if ($submission->get('utm_medium')){
+					$medium = $submission->get('utm_medium');
+				}
+			}
+			$params['utm_medium'] = $medium;
+			$term = null;
+			if (isset($_COOKIE['Visitor_Term__c'])) {
+				$term = $_COOKIE['Visitor_Term__c'] ;
+			}else{
+				if ($submission->get('utm_term')){
+					$term = $submission->get('utm_term');
+				}
+			}
+			$params['utm_term'] = $term;
+			$content = null;
+			if (isset($_COOKIE['Visitor_Content__c'])) {
+				$content = $_COOKIE['Visitor_Content__c'] ;
+			}else{
+				if ($submission->get('utm_content')){
+					$content = $submission->get('utm_content');
+				}
+			}
+			$params['utm_content'] = $content;
+			$aff_id = null;
+			if (isset($_COOKIE['Visitor_ID__c'])) {
+				$aff_id = $_COOKIE['Visitor_ID__c'] ;
+			}else{
+				if ($submission->get('transaction_id')){
+					$aff_id = $submission->get('transaction_id');
+				}
+			}
+			$params['transaction_id'] = $aff_id;
+			//month & Year
+			$month = $submission->get('time_in_business_month');
+			$year = $submission->get('time_in_business_year');
+			if ($month && $year) {
+				$params['company_started'] = $month.'/01/'.$year;
+			}
+
+			//******************* OPF API ********************************//
+			$url = env('API_URL') . env('API_VERSION') . '/leads?api_token='.env('API_TOKEN');
+			$api_response = $this->curl_api($url,"PUT",json_encode($params));
+			$api_response = json_decode($api_response,true);
+			if (isset($api_response['id'])){
+				$submission->set('lead_id',$api_response['id']);
+				$submission->save();
+			}
+		}
+		if ($form_name === 'contact'){
+			$params = array();
+			$params['fname'] = $submission->get('firstname');
+			$params['lname'] = $submission->get('lastname');
+			$params['phone'] = $submission->get('phone');
+			$params['email'] = $submission->get('email');
+			$params['company_name'] = $submission->get('business_name');
+			$params['source'] = $submission->get('source');
+			//campaign
+			$campaign = null;
+			if (isset($_COOKIE['Visitor_Campaign__c'])) {
+				$campaign = $_COOKIE['Visitor_Campaign__c'] ;
+			} else {
+				if ($submission->get('utm_campaign')){
+					$campaign = $submission->get('utm_campaign');
+				}
+			}
+			$params['utm_campaign'] = $campaign;
+			//source
+			$source = null;
+			if (isset($_COOKIE['Source__c'])) {
+				$source = $_COOKIE['Source__c'] ;
+			}else{
+				if ($submission->get('utm_source')){
+					$source = $submission->get('utm_source');
+				}
+			}
+			$params['utm_source'] = $source;
+			//medium
+			$medium = null;
+			if (isset($_COOKIE['Visitor_Medium__c'])) {
+				$medium = $_COOKIE['Visitor_Medium__c'] ;
+			}else{
+				if ($submission->get('utm_medium')){
+					$medium = $submission->get('utm_medium');
+				}
+			}
+			$params['utm_medium'] = $medium;
+			//term
+			$term = null;
+			if (isset($_COOKIE['Visitor_Term__c'])) {
+				$term = $_COOKIE['Visitor_Term__c'] ;
+			}else{
+				if ($submission->get('utm_term')){
+					$term = $submission->get('utm_term');
+				}
+			}
+			$params['utm_term'] = $term;
+			//content
+			$content = null;
+			if (isset($_COOKIE['Visitor_Content__c'])) {
+				$content = $_COOKIE['Visitor_Content__c'] ;
+			}else{
+				if ($submission->get('utm_content')){
+					$content = $submission->get('utm_content');
+				}
+			}
+			$params['utm_content'] = $content;
+			//aff_id
+			$aff_id = null;
+			if (isset($_COOKIE['Visitor_ID__c'])) {
+				$aff_id = $_COOKIE['Visitor_ID__c'] ;
+			}else{
+				if ($submission->get('transaction_id')){
+					$aff_id = $submission->get('transaction_id');
+				}
+			}
+			$params['transaction_id'] = $aff_id;
+			//push to api
+			$url = env('API_URL') . env('API_VERSION') . '/leads?api_token='.env('API_TOKEN');
+			$api_response = $this->curl_api($url,"PUT",json_encode($params));
+			$api_response = json_decode($api_response,true);
+			if (isset($api_response['id'])){
+				$submission->set('lead_id',$api_response['id']);
+				$submission->save();
+			}
+		}
+		if ($form_name === 'careers'){
+			$url = env('API_URL') . env('API_VERSION') . '/submissions?api_token='.env('API_TOKEN');
+			$this->curl_api($url,"PUT",json_encode($submission->data()));
+		}
+		return $submission;
+	}
+	//Curl
+	private function curl_api($url,$method,$post_fields){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL,$url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST,$method);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'Content-Type: application/json'
+		));
+		curl_setopt($ch, CURLOPT_POSTFIELDS,$post_fields);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+
+	//FormStack API to be removed
 	private function api_formstack_form1($submission) {
 		$form_id = '2925351';
 		$params = array();
@@ -234,7 +441,7 @@ class APIAddonListener extends Listener {
 		if (isset($result['submissions'])){
 			foreach ($result['submissions'] as $index=>$submission ) {
 				foreach ($submission['data'] as $key=>$field ) {
-					if ($key == "59894897") {
+					if ($key == "59894897" || $key == "44903891") {
 						if ($field['value'] == trim($email)){
 							return true;
 						}
@@ -243,229 +450,5 @@ class APIAddonListener extends Listener {
 			}
 		}
 		return false;
-	}
-	//OPF API
-	public function EntrySaved(EntrySaved $event) {
-		$context = $event->contextualData();
-		if ($context['collection'] === 'careers'){
-			$context['entry_id'] = $context['id'];
-			$context['status_id'] = $context['status'];
-			$context['office_id'] = $context['office'];
-			$response = $this->api_career_save($context);
-		}
-		return $event;
-	}
-	public function EntryDeleted(EntryDeleted $event) {
-		$context = $event->contextualData();
-		$this->api_career_delete($context['id']);
-		return $event;
-	}
-	public function submissionCreated(Submission $submission) {
-		$form_name = $submission->formset()->name();
-		if ($form_name === 'careers'){
-			$this->api_submission_created($submission->data());
-		}
-		if ($form_name === 'landing-page'){
-			$params = array();
-			$name = $submission->get('name');
-			$names = explode(' ', $name);
-			$revenue = $submission->get('monthly_revenue');
-			if ($revenue == 1){
-				$revenue = 2500;
-			}
-			$params['fname'] = (isset($names[0])) ? $names[0] : null;
-			$params['lname'] = (isset($names[1])) ? $names[1] : 'lastname';
-			$params['phone'] = $submission->get('phone');
-			$params['company_phone'] = $submission->get('phone');
-			$params['email'] = $submission->get('email');
-			$params['company_name'] = $submission->get('business_name');
-			$params['company_has_banking'] = $submission->get('has_business_bank_account');
-			$params['company_active_bankruptcy'] = $submission->get('active_bankruptcy');
-			$params['company_revenue'] = $revenue;
-			$params['source'] = $submission->get('source');
-			//campaign
-			$campaign = null;
-			if (isset($_COOKIE['Visitor_Campaign__c'])) {
-				$campaign = $_COOKIE['Visitor_Campaign__c'] ;
-			} else {
-				if ($submission->get('utm_campaign')){
-					$campaign = $submission->get('utm_campaign');
-				}
-			}
-			$params['utm_campaign'] = $campaign;
-			//source
-			$source = null;
-			if (isset($_COOKIE['Source__c'])) {
-				$source = $_COOKIE['Source__c'] ;
-			}else{
-				if ($submission->get('utm_source')){
-					$source = $submission->get('utm_source');
-				}
-			}
-			$params['utm_source'] = $source;
-			//medium
-			$medium = null;
-			if (isset($_COOKIE['Visitor_Medium__c'])) {
-				$medium = $_COOKIE['Visitor_Medium__c'] ;
-			}else{
-				if ($submission->get('utm_medium')){
-					$medium = $submission->get('utm_medium');
-				}
-			}
-			$params['utm_medium'] = $medium;
-			//Aff ID
-			$aff_id = null;
-			if (isset($_COOKIE['Visitor_ID__c'])) {
-				$aff_id = $_COOKIE['Visitor_ID__c'] ;
-			}else{
-				if ($submission->get('transaction_id')){
-					$aff_id = $submission->get('transaction_id');
-				}
-			}
-			$params['transaction_id'] = $aff_id;
-			//term
-			$term = null;
-			if (isset($_COOKIE['Visitor_Term__c'])) {
-				$term = $_COOKIE['Visitor_Term__c'] ;
-			}else{
-				if ($submission->get('utm_term')){
-					$term = $submission->get('utm_term');
-				}
-			}
-			$params['utm_term'] = $term;
-			//content
-			$content = null;
-			if (isset($_COOKIE['Visitor_Content__c'])) {
-				$content = $_COOKIE['Visitor_Content__c'] ;
-			}else{
-				if ($submission->get('utm_content')){
-					$content = $submission->get('utm_content');
-				}
-			}
-			$params['utm_content'] = $content;
-			//month & Year
-			$month = $submission->get('time_in_business_month');
-			$year = $submission->get('time_in_business_year');
-			if ($month && $year) {
-				$params['company_started'] = $month.'/01/'.$year;
-			}
-			$api_response = $this->api_lead_created($params);
-			$api_response = json_decode($api_response,true);
-			if (isset($api_response['id'])){
-				$submission->set('lead_id',$api_response['id']);
-				$submission->save();
-			}
-		}
-		if ($form_name === 'contact'){
-			$params = array();
-			$params['fname'] = $submission->get('firstname');
-			$params['lname'] = $submission->get('lastname');
-			$params['phone'] = $submission->get('phone');
-			$params['email'] = $submission->get('email');
-			$params['company_name'] = $submission->get('business_name');
-			$params['source'] = $submission->get('source');
-			//campaign
-			$campaign = null;
-			if (isset($_COOKIE['Visitor_Campaign__c'])) {
-				$campaign = $_COOKIE['Visitor_Campaign__c'] ;
-			} else {
-				if ($submission->get('utm_campaign')){
-					$campaign = $submission->get('utm_campaign');
-				}
-			}
-			$params['utm_campaign'] = $campaign;
-			//source
-			$source = null;
-			if (isset($_COOKIE['Source__c'])) {
-				$source = $_COOKIE['Source__c'] ;
-			}else{
-				if ($submission->get('utm_source')){
-					$source = $submission->get('utm_source');
-				}
-			}
-			$params['utm_source'] = $source;
-			//medium
-			$medium = null;
-			if (isset($_COOKIE['Visitor_Medium__c'])) {
-				$medium = $_COOKIE['Visitor_Medium__c'] ;
-			}else{
-				if ($submission->get('utm_medium')){
-					$medium = $submission->get('utm_medium');
-				}
-			}
-			$params['utm_medium'] = $medium;
-			//Aff ID
-			$aff_id = null;
-			if (isset($_COOKIE['Visitor_ID__c'])) {
-				$aff_id = $_COOKIE['Visitor_ID__c'] ;
-			}else{
-				if ($submission->get('transaction_id')){
-					$aff_id = $submission->get('transaction_id');
-				}
-			}
-			$params['transaction_id'] = $aff_id;
-			//term
-			$term = null;
-			if (isset($_COOKIE['Visitor_Term__c'])) {
-				$term = $_COOKIE['Visitor_Term__c'] ;
-			}else{
-				if ($submission->get('utm_term')){
-					$term = $submission->get('utm_term');
-				}
-			}
-			$params['utm_term'] = $term;
-			//content
-			$content = null;
-			if (isset($_COOKIE['Visitor_Content__c'])) {
-				$content = $_COOKIE['Visitor_Content__c'] ;
-			}else{
-				if ($submission->get('utm_content')){
-					$content = $submission->get('utm_content');
-				}
-			}
-			$params['utm_content'] = $content;
-			//month & Year
-			$api_response = $this->api_lead_created($params);
-			$api_response = json_decode($api_response,true);
-			if (isset($api_response['id'])){
-				$submission->set('lead_id',$api_response['id']);
-				$submission->save();
-			}
-		}
-		return $submission;
-	}
-	private function api_career_save($career){
-		$url = env('API_URL') . env('API_VERSION') . '/careers?api_token='.env('API_TOKEN');
-		$method = "PUT";
-		return $this->sync($url,$method,json_encode($career));
-	}
-	private function api_career_delete($entry_id){
-		$url = env('API_URL') . env('API_VERSION') . '/careers/' . $entry_id . '?api_token='.env('API_TOKEN');
-		$method = "DELETE";
-		return $this->sync($url,$method,$entry_id);
-	}
-	private function api_submission_created($submission){
-		$url = env('API_URL') . env('API_VERSION') . '/submissions?api_token='.env('API_TOKEN');
-		$method = "PUT";
-		return $this->sync($url,$method,json_encode($submission));
-	}
-	private function api_lead_created($data){
-		$url = env('API_URL') . env('API_VERSION') . '/leads?api_token='.env('API_TOKEN');
-		$method = "PUT";
-		return $this->sync($url,$method,json_encode($data));
-	}
-	private function sync($url,$method,$post_fields){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL,$url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST,$method);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-			'Content-Type: application/json'
-		));
-		curl_setopt($ch, CURLOPT_POSTFIELDS,$post_fields);
-		$data = curl_exec($ch);
-		curl_close($ch);
-		return $data;
 	}
 }
