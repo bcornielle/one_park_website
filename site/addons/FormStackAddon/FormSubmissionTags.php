86ed81eb882c22a9f34a77f66dd346dd4a0e6cca
Krodox <julian@krodox.com>
2019-05-15 23:12:29 -0400

    All in one Add-on fix

--- site/addons/FormStackAddon/FormSubmissionTags.php
+++ site/addons/FormStackAddon/FormSubmissionTags.php
@@ -8,5 +8,10 @@ class FormSubmissionTags extends Tags {
 			$e->setUrl(session('form_stack_redirect'));
 			throw $e;
 		}
+		if (session('lead_redirect')) {
+			$e = new \Statamic\Exceptions\RedirectException;
+			$e->setUrl(session('lead_redirect'));
+			throw $e;
+		}
 	}
 }
