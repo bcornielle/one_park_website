8b2d8a9e08af7d9b6276bb2d78cf163c82e8b5f7
Krodox <julian@krodox.com>
2019-09-04 10:55:31 -0400

    Welcoming Webpack (Finally)

--- site/themes/krodox/webpack.mix.js
+++ site/themes/krodox/webpack.mix.js
@@ -2,7 +2,6 @@ const mix = require("laravel-mix");
 const elixir = require('laravel-elixir');
 elixir.config.assetsPath = './';
 elixir.config.publicPath = './';
-
 mix.scripts(['js/vendor/collage-plus.js','js/vendor/zoom.js','js/jabbascripts.js','js/vue.js'],'js/app.js')
     .sass("sass/style.scss", "css")
     .options({
